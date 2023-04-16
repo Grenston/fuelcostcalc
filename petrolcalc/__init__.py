5ed0373c772c45985f0aec1cbd8cf01e9d910c7a
Grenston <grenston.ks@gmail.com>
2023-04-16 23:13:27 +0530

    work started

--- petrolcalc/__init__.py
+++ petrolcalc/__init__.py
@@ -1,22 +1,42 @@
 import logging
 
 import azure.functions as func
-
+import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    URL = 'https://www.ndtv.com/fuel-prices/petrol-price-in-all-state'
 
-    name = req.params.get('name')
-    if not name:
+    distance = req.params.get('distance')
+    if not distance:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            distance = req_body.get('distance')
+        distance = req.params.get('distance')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    mileage = req.params.get('mileage')
+    if not mileage:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            mileage = req_body.get('mileage')
+
+    state = req.params.get('state')
+    if not state:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            state = 'Kerala'
+        else:
+            state = req_body.get('state')   
+
+    if distance and mileage:
+        return func.HttpResponse(json.dumps({"distance":distance,"mileage":mileage,"state":state}),mimetype="application/json",status_code=200)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
